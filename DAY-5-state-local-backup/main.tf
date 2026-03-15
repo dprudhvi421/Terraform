da21dfc7111ae76c5f54b2bac8649f2fc35bec42
prudhvi <dprudhvi421@gmail.com>
2026-03-15 17:26:42 +0530

    DAY-5-backup-state-file

---
+++ DAY-5-state-local-backup/main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "test"
+    }
+}
+resource "aws_instance" "test" {
+    ami = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t3.micro"
+}
